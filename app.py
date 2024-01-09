a7c9dc60bc972fd8c8ad495741c8aee56d1505a2
Lakshmi priya <155796236+lakshmipriya45@users.noreply.github.com>
2024-01-09 11:07:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 dataset=pd.read_csv('data.csv')
+data=dataset.iloc[:,0].values
 
 st.title('User Driven Review System')
 
 text=st.text_input('Enter the website')
 
 if st.button('Predict'):
+  if text in list(data):
+    st.success('Original')
+  else:
+    st.error('Fake')
   
 
